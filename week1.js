086c8a2c292a993b4ded9f6df0212064999993d4
zxn16 <alamzain9999@gmail.com>
2024-12-16 16:59:22 +0000

    Add files via upload

---
+++ week1.js
@@ -0,0 +1,11 @@
+console.log("this is my first programe")
+console.log("welcome zain your month salary is £100000")
+
+
+const num1 =5;
+const num2 =3;
+//add two numbers
+const sum = num1 + num2
+//display the sum
+
+console.log('The sum of' +num1 + 'and' + num2+ 'is: ' +sum)
